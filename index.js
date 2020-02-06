2d66bee37451c50c4212ddad61ef5f9d4fb68fd4
Patrick Rathmann <rhistan@gmail.com>
2020-02-06 10:58:54 +0100

    feat: include bundle paramater

--- index.js
+++ index.js
@@ -1,98 +1,106 @@
 'use strict';
-var path = require('path');
-var fs = require('fs');
-var os = require('os');
-var through = require('through2');
-var gutil = require('gulp-util');
-var Q = require('q');
-var cordova = require('cordova-lib').cordova;
+const path = require('path');
+const fs = require('fs');
+const os = require('os');
+const through = require('through2');
+const gutil = require('gulp-util');
+const glob = require('glob');
+const {cordova} = require('cordova-lib');
 
-// export the module
 module.exports = function (options) {
 	options = options || {};
 
-	return through.obj(function (file, enc, cb) {
+	return through.obj((file, enc, cb) => {
 		// Change the working directory
 		process.env.PWD = file.path;
 
 		cb();
 	}, function (cb) {
-		var self = this;
-		var androidPath = path.join(cordova.findProjectRoot(), 'platforms', 'android');
-		var sign = options.storeFile && options.keyAlias;
-		var release = options.release || sign;
-		var buildMethod = options.buildMethod || process.env.ANDROID_BUILD;
+		const androidPath = path.join(cordova.findProjectRoot(), 'platforms', 'android');
+		const sign = options.storeFile && options.keyAlias;
+		const release = options.release || sign;
+		const buildMethod = options.buildMethod || process.env.ANDROID_BUILD;
+		const bundle = options.bundle !== undefined ? options.bundle : false;
 
-		Q.fcall(function () {
-			return fs.existsSync(androidPath);
-		}).then(function (exists) {
-			if (!exists) {
-				// Add the android platform if it does not exist
-				return cordova.platforms('add', 'android' + (options.version ? ('@' + options.version) : ''),
-							 { fetch: true });
-			}
-		}).then(function () {
-			if (sign) {
-				var data = [];
+		const exists = fs.existsSync(androidPath);
 
-				// Add all the options related to key signing to the array to be added to 'release-signing.properties'
-				data.push('storeFile=' + options.storeFile);
-				data.push('keyAlias=' + options.keyAlias);
-				if (options.storePassword) {
-					data.push('storePassword=' + options.storePassword);
+		Promise.resolve()
+			.then(() => {
+				if (!exists) {
+					// Add the android platform if it does not exist
+					return cordova.platforms('add', 'android' + (options.version ? ('@' + options.version) : ''));
 				}
-				if (options.keyPassword) {
-					data.push('keyPassword=' + options.keyPassword);
+			})
+			.then(() => {
+				if (sign) {
+					const data = [];
+
+					// Add all the options related to key signing to the array to be added to 'release-signing.properties'
+					data.push('storeFile=' + options.storeFile);
+					data.push('keyAlias=' + options.keyAlias);
+
+					if (options.storePassword) {
+						data.push('storePassword=' + options.storePassword);
+					}
+
+					if (options.keyPassword) {
+						data.push('keyPassword=' + options.keyPassword);
+					}
+
+					if (options.storeType) {
+						data.push('storeType=' + options.storeType);
+					}
+
+					// Write the release-signing.properties file
+					fs.writeFileSync(path.join(androidPath, 'release-signing.properties'), data.join(os.EOL));
 				}
-				if (options.storeType) {
-					data.push('storeType=' + options.storeType);
+			})
+			.then(() => {
+				const options = [];
+
+				if (release) {
+					// If the user wants to build for release, add the option
+					options.push('--release');
 				}
 
-				// Write the release-signing.properties file
-				fs.writeFileSync(path.join(androidPath, 'release-signing.properties'), data.join(os.EOL));
-			}
-		}).then(function () {
-			var options = {};
+				if (buildMethod === 'ant') {
+					options.push('--ant');
+				} else {
+					options.push('--gradle');
+				}
 
-			if (release) {
-				// If the user wants to build for release, add the option
-				options.release = true;
-			}
-			
-			if (buildMethod === 'ant') {
-				options.ant = true;
-			} 
-			// the else case is obsolete now, because the correct option would be 'studio = true'
-			// which we don't actually need to set, because cordova correctly detects that itself
+				if (bundle) {
+					options.push('--packageType=bundle');
+				}
 
-			// Build the platform
-			return cordova.build({platforms: ['android'], options: options});
-		}).then(function () {
-			var apkOutputPath = buildMethod === 'ant' ? 'bin' : (release ? 'app/build/outputs/apk/release' : 'app/build/outputs/apk/debug');
-			var base = path.join(androidPath, apkOutputPath);
-			var cwd = process.env.PWD;
+				// Build the platform
+				return cordova.build({platforms: ['android'], options});
+			})
+			.then(() => {
+				const apkOutputPath = buildMethod === 'ant' ? 'bin' : 'app/build/outputs/apk';
+				const base = path.join(androidPath, apkOutputPath);
+				const cwd = process.env.PWD;
 
-			// Iterate over the output directory
-			fs.readdirSync(base).forEach(function (file) {
-				// Check if the file ends with .apk
-				if (file.indexOf('.apk') !== -1) {
-					var filePath = path.join(base, file);
+				const files = glob.sync('**/*.apk', {cwd: base});
+
+				for (const file of files) {
+					const filePath = path.join(base, file);
 
 					// Push the file to the result set
-					self.push(new gutil.File({
-						base: base,
-						cwd: cwd,
+					this.push(new gutil.File({
+						base,
+						cwd,
 						path: filePath,
 						contents: fs.readFileSync(path.join(base, file))
 					}));
 				}
-			});
 
-			cb();
-		}).catch(function (err) {
-			console.log(err);
-			// Return an error if something happened
-			cb(new gutil.PluginError('gulp-cordova-build-android', err.message));
-		});
+				cb();
+			})
+			.catch(error => {
+				console.error(error);
+				// Return an error if something happened
+				cb(new gutil.PluginError('gulp-cordova-build-android', error.message));
+			});
 	});
 };
